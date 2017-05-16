019fca5eccf5eea2fd2367f9b3b1eb0b3ff7d731
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-16 08:48:30 -0700

    change app name when calling using models in account controller

--- src/InterpretGO/Controllers/AccountController.cs
+++ src/InterpretGO/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using InterpretGO.Models;
 using System.Threading.Tasks;
-using BasicAuthentication.ViewModels;
+using InterpretGO.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
