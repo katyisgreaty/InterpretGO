d5dc962f148f30a6b668485f02d5bc1e234bc8f8
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-17 16:23:30 -0700

    add most of functionality for assignment

---
+++ src/InterpretGO/Controllers/AssignmentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InterpretGO.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InterpretGO.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        // GET: /<controller>/
+        private InterpretGODbContext db = new InterpretGODbContext();
+        public IActionResult Index()
+        {
+            return View(db.Assignments.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisAssignment = db.Assignments.FirstOrDefault(assignments => assignments.AssignmentId == id);
+            return View(thisAssignment);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Assignment assignment)
+        {
+            db.Assignments.Add(assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisAssignment = db.Assignments.FirstOrDefault(assignments => assignments.AssignmentId == id);
+            return View(thisAssignment);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Assignment assignment)
+        {
+            db.Entry(assignment).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisAssignment = db.Assignments.FirstOrDefault(assignments => assignments.AssignmentId == id);
+            return View(thisAssignment);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisAssignment = db.Assignments.FirstOrDefault(assignments => assignments.AssignmentId == id);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
