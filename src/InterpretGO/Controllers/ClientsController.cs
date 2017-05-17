876a8a4861f3e88ce88ef8fead393b11922c6205
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-17 15:20:33 -0700

    add create functionality for clients

---
+++ src/InterpretGO/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InterpretGO.Models;
+
+namespace InterpretGO.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private InterpretGODbContext db = new InterpretGODbContext();
+        public IActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Client client)
+        {
+            db.Clients.Add(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
