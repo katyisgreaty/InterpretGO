c2f02ae11d0a276ba897713870c8c35dc10cd4b3
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-17 15:31:07 -0700

    add complete crud functionality for client

--- src/InterpretGO/Controllers/ClientsController.cs
+++ src/InterpretGO/Controllers/ClientsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using InterpretGO.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InterpretGO.Controllers
 {
@@ -15,6 +16,12 @@ namespace InterpretGO.Controllers
             return View(db.Clients.ToList());
         }
 
+        public IActionResult Details(int id)
+        {
+            var thisClient = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            return View(thisClient);
+        }
+
         public IActionResult Create()
         {
             return View();
@@ -24,6 +31,35 @@ namespace InterpretGO.Controllers
         public IActionResult Create(Client client)
         {
             db.Clients.Add(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisClient = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Client client)
+        {
+            db.Entry(client).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisClient = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisClient = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            db.Clients.Remove(thisClient);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
