0bf6325b331864137a0c56495ce405aeace27481
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-07-17 14:40:04 -0700

    Add personal and bootstrap css files, and begin to adjust date property for assignment

--- src/InterpretGO/Startup.cs
+++ src/InterpretGO/Startup.cs
@@ -38,6 +38,7 @@ namespace InterpretGO
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
             app.UseIdentity();
             app.UseMvc(routes =>
             {
